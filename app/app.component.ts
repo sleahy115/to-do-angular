42e5e1c39914467ef0dcef9575bd6b41a363e57c
jay freeman and Sarah Leahy <sarah@>
2017-03-27 16:47:51 -0700

    output tasks on homepage

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1>To-do list for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+      <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
+    </ul>
+  </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JavaScript group projects'),
+    new Task('Add README file to last few Angular repos on GitHub')
+  ];
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
